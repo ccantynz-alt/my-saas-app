0529d0605adc20adf416b4b2f713a9a02b9aa01f
ccantynz-alt <ccantynz@gmail.com>
2025-12-23 12:53:09 +1300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <main style={{ padding: 40 }}>
-      <h1>My SaaS app is live 🚀</h1>
+      <h1>It’s working ✅🚀</h1>
+
       <p>Next.js is now working on Vercel.</p>
     </main>
   );
