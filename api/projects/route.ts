7d053873ff61f74fbc80f736ec4d174014176106
ccantynz-alt <ccantynz@gmail.com>
2025-12-25 07:08:47 +1300

    Create route.ts

---
+++ api/projects/route.ts
@@ -0,0 +1,5 @@
+import { KV } from "../../lib/kv";
+import { keys } from "../../lib/keys";
+import { makeId } from "../../lib/ids";
+import { getCurrentUserId } from "../../lib/demoAuth";
+import { CreateProjectInputSchema, ProjectSchema } from "../../lib/models/project";
