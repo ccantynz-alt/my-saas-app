bbf788d4f5a59a6ca9dd61ecd2806c328866de6e
ccantynz-alt <ccantynz@gmail.com>
2025-12-25 07:12:41 +1300

    Delete api/projects directory

--- api/projects/route.ts
+++
@@ -1,5 +0,0 @@
-import { KV } from "../../lib/kv";
-import { keys } from "../../lib/keys";
-import { makeId } from "../../lib/ids";
-import { getCurrentUserId } from "../../lib/demoAuth";
-import { CreateProjectInputSchema, ProjectSchema } from "../../lib/models/project";
