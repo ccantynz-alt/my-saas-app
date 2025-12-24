6b315cfe917eaaa2dbdfc442b73af85afdffacff
ccantynz-alt <ccantynz@gmail.com>
2025-12-24 23:19:55 +1300

    Update route.ts

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,12 +1,9 @@
 import { NextResponse } from "next/server";
-import { KV } from "../../../lib/kv";
-import { keys } from "../../../lib/keys";
-import { makeId } from "../../../lib/ids";
-import { getCurrentUserId } from "../../../lib/demoAuth";
-import {
-  CreateProjectInputSchema,
-  ProjectSchema,
-} from "../../../lib/models/project";
+import { KV } from "../../lib/kv";
+import { keys } from "../../lib/keys";
+import { makeId } from "../../lib/ids";
+import { getCurrentUserId } from "../../lib/demoAuth";
+import { CreateProjectInputSchema, ProjectSchema } from "../../lib/models/project";
 
 export const runtime = "nodejs";
 
