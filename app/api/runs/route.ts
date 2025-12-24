962f820f7d2f56b510ab554b7187cde297812427
ccantynz-alt <ccantynz@gmail.com>
2025-12-24 23:20:54 +1300

    Update route.ts

--- app/api/runs/route.ts
+++ app/api/runs/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
-import { KV } from "../../../lib/kv";
-import { keys } from "../../../lib/keys";
-import { makeId } from "../../../lib/ids";
-import { getCurrentUserId } from "../../../lib/demoAuth";
-import { CreateRunInputSchema, RunSchema } from "../../../lib/models/run";
-import { RunLogSchema } from "../../../lib/models/log";
+import { KV } from "../../lib/kv";
+import { keys } from "../../lib/keys";
+import { makeId } from "../../lib/ids";
+import { getCurrentUserId } from "../../lib/demoAuth";
+import { CreateRunInputSchema, RunSchema } from "../../lib/models/run";
+import { RunLogSchema } from "../../lib/models/log";
 
 export const runtime = "nodejs";
 
