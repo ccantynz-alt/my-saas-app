6d92fe5e8a6293cbd524318433fd62c17b0bb420
ccantynz-alt <ccantynz@gmail.com>
2025-12-26 01:24:20 +1300

    Delete pages directory

--- pages/api/auth/logout.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { authClearCookieHeader } from "../../../lib/auth";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") return res.status(405).json({ ok: false, message: "Method not allowed" });
-
-  res.setHeader("Set-Cookie", authClearCookieHeader());
-  return res.status(200).json({ ok: true });
-}
