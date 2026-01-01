919ef47bf2cbc5b85154a63f6dd1b7e1eaedfaea
ccantynz-alt <ccantynz@gmail.com>
2026-01-01 17:05:32 +1300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,10 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/admin(.*)",
-  "/studio(.*)",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Next.js recommended matcher
-    "/((?!_next|.*\\.(?:css|js|json|png|jpg|jpeg|gif|svg|ico|webp|txt|xml)).*)",
+    "/((?!_next|.*\\..*).*)",
     "/(api|trpc)(.*)",
   ],
 };
