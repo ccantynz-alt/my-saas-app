317adeb47b7ad82432f5c9edebf632c6d14cf462
ccantynz-alt <ccantynz@gmail.com>
2026-01-02 23:11:12 +1300

    Update route.ts

--- app/site/[projectId]/sitemap.xml/route.ts
+++ app/site/[projectId]/sitemap.xml/route.ts
@@ -10,18 +10,6 @@ export async function GET(
     .map(
       (p) =>
         `<url><loc>${process.env.NEXT_PUBLIC_BASE_URL}/site/${params.projectId}/${p.slug}</loc></url>`
-    )import { getSeoPages } from "@/app/lib/seoKV";
-
-export async function GET(
-  _: Request,
-  { params }: { params: { projectId: string } }
-) {
-  const pages = await getSeoPages(params.projectId);
-
-  const urls = pages
-    .map(
-      (p) =>
-        `<url><loc>${process.env.NEXT_PUBLIC_BASE_URL}/site/${params.projectId}/${p.slug}</loc></url>`
     )
     .join("");
 
@@ -31,18 +19,8 @@ ${urls}
 </urlset>`;
 
   return new Response(xml, {
-    headers: { "Content-Type": "application/xml" },
-  });
-}
-
-    .join("");
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls}
-</urlset>`;
-
-  return new Response(xml, {
-    headers: { "Content-Type": "application/xml" },
+    headers: {
+      "Content-Type": "application/xml",
+    },
   });
 }
