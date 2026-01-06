f5f886a564f0ced1819ca3cf38a08909127a5a4b
ccantynz-alt <ccantynz@gmail.com>
2026-01-07 06:48:53 +1300

    Add version history + rollback for generated HTML

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -1,4 +1,6 @@
+import GeneratePanel from "./GeneratePanel";
 import PublishButton from "./PublishButton";
+import VersionsPanel from "./VersionsPanel";
 import RunDemoPanel from "./RunDemoPanel";
 import DomainPanel from "./DomainPanel";
 
@@ -14,7 +16,11 @@ export default async function ProjectPage({
       <h1 style={{ margin: 0, marginBottom: 8 }}>Project</h1>
       <div style={{ opacity: 0.75, marginBottom: 16 }}>{projectId}</div>
 
+      <GeneratePanel projectId={projectId} />
       <PublishButton projectId={projectId} />
+
+      {/* ✅ NEW: Version history + rollback */}
+      <VersionsPanel projectId={projectId} />
 
       <div style={{ marginTop: 24 }}>
         <RunDemoPanel projectId={projectId} />
@@ -26,4 +32,3 @@ export default async function ProjectPage({
     </main>
   );
 }
-
