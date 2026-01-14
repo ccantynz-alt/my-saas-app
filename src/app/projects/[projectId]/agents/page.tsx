513956e5bbb601c1f2f795942081819c4d8801f6
ccantynz-alt <ccantynz@gmail.com>
2026-01-15 06:01:57 +1300

    Create page.tsx

---
+++ src/app/projects/[projectId]/agents/page.tsx
@@ -0,0 +1,9 @@
+import AgentsClient from "./AgentsClient";
+
+type Props = {
+  params: { projectId: string };
+};
+
+export default function AgentsPage({ params }: Props) {
+  return <AgentsClient projectId={params.projectId} />;
+}
