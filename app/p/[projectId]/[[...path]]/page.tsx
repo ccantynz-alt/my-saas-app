958a15a7bdec7dac68cb4b38a828061477845340
ccantynz-alt <ccantynz@gmail.com>
2026-01-17 08:01:20 +1300

    Update page.tsx

--- app/p/[projectId]/[[...path]]/page.tsx
+++ app/p/[projectId]/[[...path]]/page.tsx
@@ -1,2 +1,3 @@
-export { default } from "../page";
-export const runtime = "nodejs";
+// app/p/[projectId]/[[...path]]/page.tsx
+// Catch-all published route: reuse the same renderer as /p/[projectId]
+export { default } from "../../page";
